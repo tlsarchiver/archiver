76595c7e5e1e795a83e02f106c96715d1b6dfaaa
Anatole Denis <anatole.denis@polyconseil.fr>
2016-06-30 13:48:58 +0200

    Fix indenting in DB creation script

--- initdb.sql
+++ initdb.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `certificates` (
     `id`                INTEGER NOT NULL UNIQUE,
-	`host`	            TEXT NOT NULL,
+    `host`              TEXT NOT NULL,
     `ip`                TEXT,
-	`ciphersuite`	    INT,
-	`protocol`	        INT,
+    `ciphersuite`       INT,
+    `protocol`          INT,
     `certificate_idx`   INT,
     `certificate_raw`   TEXT,
     `failed`            BOOLEAN DEFAULT false,
     `failure_error`     TEXT,
     `timestamp`         DATETIME NOT NULL,
-	PRIMARY KEY(`id`)
+    PRIMARY KEY(`id`)
 );
